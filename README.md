3aa1148ca60dc050c9e9005f3ed6d0f03f0d0fae
RediiChen <a0929052958@gmail.com>
2021-04-28 21:49:35 +0800

    優化js程式碼＆更新readme

--- README.md
+++ README.md
@@ -1,29 +1,42 @@
-# WD_prac2102_Scroll2Elem
-捲動功能的實現
+# <center>Scroll2Elem</center>
+## 功能介紹
+- 為同頁的連結提供「捲動效果」
+- 提供to the top(返回最上)的功能
 
-# 功能介紹
-- 實現一頁式連結的滾動效果
-- 返回最上的箭頭按鈕
-
-# 使用說明。
-## 連結CDN
-連結此CDN之前，請確認也已經連結jQuery的函式庫。
-接著即可將下列的link code置於body結束標籤之前。
+## 使用說明
+### 1. 連結CDN
+連結此CDN之前，需要先連結jQuery的函式庫。<br>
+你可以將下列的link code置於結束標籤`</body>`之前。
 ```
-    <script src="https://rediichen.github.io/WD_prac2102_ScrollToElem/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://rediichen.github.io/WD_prac2102_ScrollToElem/main.js"></script>
 ```
 
-## 滾動前往功能
-效果名稱 | 屬性名稱 | 賦值內容/單位
---------|---------|-----------------------
-指定連結的目標 | data-s2-target | 元素id(不用#)
-設定效果的時間長 | data-s2-duration | 毫秒
-設定移動目標的位置修正 | data-s2-offset | px
+### 2. 捲動前往功能
+1. 將想要前往的目標元素取id名稱：
+```
+<div id="target-elem"></div>
+```
+2. 在連結的按鈕元素的html屬性中設定：
+```
+<a data-s2-target="target-elem" data-s2-offset="-100"> ... </a>
+```
 
-## 返回最上方功能
-你可以自行設計箭頭按鈕的樣式，
-但請務必將其id設定為```arrow```。
-效果名稱 | 屬性名稱 | 賦值內容/資料型態/單位
---------|---------|-----------------------
-要離最上方多遠才顯示出按鈕 | data-s2-top | px
-顯示/消失的效果時間長 | data-s2-time | 毫秒
+
+效果名稱 | 屬性名稱 | 賦值內容/單位 | 必填/預設值
+:------:|:------:|:------------:|:---------:
+指定連結的目標 | data-s2-target | 元素id(不用#) | 必填
+設定移動目標的位置修正 | data-s2-offset | px(向下為正) | 0
+
+### 3. 返回最上方功能
+只需要將你的元素id設定為`tothetop`：
+```
+<div id="tothetop"> ... </div>
+```
+箭頭按鈕的樣式你可以自行設計。<br>
+tothetop的滾動前往功能預設是回到最上方，<br>
+如果你希望調整offset，則請採用上方 2. 的步驟。
+效果名稱 | 屬性名稱 | 如何給值 | 單位 | 必填/預設值
+:------:|:-------:|:------:|:----:|:---------:
+離最上方多遠時<br>開始讓按鈕顯示 | data-s2-top | 整數 | px | 500 (px)
+顯示/消失的效果時間長 | data-s2-time | 整數 | 毫秒 | 800 (ms)
--- main.js
+++ main.js
@@ -1,70 +1,65 @@
 $("a").each(function () {
-    //(0) each函數是遍歷全部同類元素的功能
-    //(1) 雖然 a 是內建的詞彙，但是仍要使用雙引號
-    //(2) 原function (i, elem)當中的 i 在此功能不會用到
-    //    而 elem 是當前的元素，但以下使用 "this" 選擇器
-    //    因此括弧內省略
     //* Stage I : 收集參數
     var target = $(this).attr("data-s2-target");
-    var duration = $(this).attr("data-s2-duration");
-    var offsetTop = $(this).attr("data-s2-offset");
-    //(1) 用attr讀取在html中設定的屬性值
-    //再用變數存放
-    //(2) this 是方便當下存取的代名詞，程式裡是一個全域的變數。
-    // 一旦存取了別的變數，this 就會變成代表別人了。
+    if (!target) return true;// 沒有設定此參數的就直接換下一位
 
-    //* 測試info是否取得成功
-    // console.log("");
-    //為了讓訊息呈現有分段效果
-    // console.log("編號：" + i);
-    // console.log("目標：" + target);
-    // console.log("時長：" + duration);
-    // console.log("座標修正：" + offsetTop);
+    var position = $("#" + target).offset();
+    // (1) offset 元素在網頁上的座標
 
-    var offset = $("#" + target).offset();
-    //
-    var top = offset.top;
-    //用if篩選，offsetTop存在才執行
-    if (offsetTop) top -= offsetTop;
+    var posAdj = parseInt($(this).attr("data-s2-offset"));
+    // (2) posAdj = position adjust 微調目標的座標
+    //TODO 希望可以自動算效果的時間
 
-    //* 測試info是否取得成功
-    // console.log("目標top座標：" + top);
+    //* Stage II : 參數操作
+    var posY = position.top;
+    if (posAdj) posY += posAdj; // 有要求才改，以免undefined礙事
+    //! 內捲動空間，會跟著算入
 
-    //* Stage II : 移動至目標
+    //* Stage III : 前往至目的地
+    //(1) UX : 插入stop() 讓點選其他按鈕可以中止機制
+    //(2) html, body 是為了跨平台的以防萬一
     $(this).click(function (e) {
         e.preventDefault();//關閉 a 原本的連結功能
-        $("html").stop().animate({
-            //1. 點錯，臨時改點另一個目標
-            //插入stop() 是中斷當前所有未完成的動畫效果
-            //UX實務：為了讓使用者可以臨時改變心意
-            scrollTop: top
-        }, parseInt(duration));
-        //因為輸入的是字串，所以需要轉換資料型態。
+        $("html, body").stop().animate({
+            scrollTop: posY
+        }, 800);
     });
-    //2. 點錯，臨時用滾輪掙扎
-    $("html").on("mousewheel", function () {
-        $("html").stop();
+    //(3) UX : 讓滾輪可以中止機制
+    $("html, body").on("mousewheel", function () {
+        $("html, body").stop();
     });
-
-
 });
-//接下來是arrow按鈕的顯示/消失設定
-var arrow = $("#arrow");
+//接下來是tothetop按鈕的顯示/消失設定
+var tothetop = $("#tothetop");
 
 $(window).scroll(function () {
-    var windowTop = $(this).scrollTop();
-    // console.log("此時上方座標：" + windowTop) ;
-    //this log幫助收集估查位置的座標
-    var top = arrow.attr("data-s2-top");
-    var time = parseInt(arrow.attr("data-s2-time"));
+    var windowPos = $(this).scrollTop();
+    // console.log("此時上方座標：" + windowPos) ;
+    //此log能幫助收集估查位置的座標
+    var borderLine = tothetop.attr("data-s2-top") || 500;
+    //顯示邊界的預設值：500px
+    var time = parseInt(tothetop.attr("data-s2-time")) || 800;
+    //顯示效果時長的預設值：800ms
     // console.log("界線座標：" + top) ;
     // console.log("設定時間：" + time) ;
 
-    if (windowTop >= top) arrow.stop().fadeIn(time);
-    else arrow.stop().fadeOut(time);
-    //UX實務：以防使用者白目上上下下玩特效，讓每次動作前的動作都取消。
+    if (windowPos >= borderLine) tothetop.stop().fadeIn(time);
+    else tothetop.stop().fadeOut(time);
+    //UX ：以防使用者白目上上下下玩特效，讓每次動作前的動作都取消。
+
+    //設定預設值
+    var target = $(this).attr("data-s2-target");
+    if(target) return 0;
+    //有採用target功能則不執行以下指令
+    $(this).click(function (e) {
+        e.preventDefault();
+        $("html, body").stop().animate({
+            scrollTop: 0
+        }, 800);
+    });
+    $("html, body").on("mousewheel", function () {
+        $("html, body").stop();
+    });
 });
-
-
     //最後的註解：console.log應在開發完畢後刪除
     //以免造成使用時浪費瀏覽器的資源。
